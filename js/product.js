f2dd1de49dee3ef6c9bef975ca5f452c448cee10
Matheus Germano de Moraes <matheusgermano97@hotmail.com>
2020-04-15 16:03:50 -0300

    a

--- js/product.js
+++ js/product.js
@@ -4,10 +4,25 @@ $(".sku-notifyme-form p").text("Avise-me quando estiver disponível");
 
 
 setTimeout(function () {
-    $(".glis-popup-link.glis-thickbox.tb-added.qvBinded", "body").html(`<img src="/arquivos/CORACAO2.png" style="max-width: 18px"> ADICIONAR A LISTA DE DESEJOS`);
+    $(".desk-info-nav a#first").css({"color":"#BEBEBE"});
+    $(".product-info .shipping-box label", "body").prepend(`<img src="/arquivos/truck.png" style="margin-right: 10px" >`)
+    $(".glis-popup-link.glis-thickbox.tb-added.qvBinded", "body").html(`<img src="/arquivos/CORACAO.png" style="max-width: 18px"> ADICIONAR A LISTA DE DESEJOS`);
     $("#TB_closeWindowButton").html(`<img src="/arquivos/menu-close.png" style="max-width:25px"/>`);
-    $(".user-review h4").text("AVALIAÇÕES");
 }, 1000);
+
+
+$(".desk-info-nav a").click((e)=>{
+    $(".desk-info-nav a").css({"color":"#BEBEBE"});
+    $(e.target).css({"color":"#000"});
+    if($(e.target).attr("id") == "first") {
+        $(".product-description-box #specification").fadeOut()
+        $(".product-description-box #description").fadeIn()
+    } else if ($(e.target).attr("id") == "last") {
+        $(".product-description-box #description").fadeOut()
+        $(".product-description-box #specification").fadeIn()
+    }
+
+})
 
 
 $(".dropdown-container").hide();
